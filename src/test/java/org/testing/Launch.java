534d3dfe8a35aca4d96e0ec2944e9c22b71a8404
divyabharathi0206 <divyabharathi7h@gmail.com>
2020-02-03 11:53:51 +0530

    task

--- src/test/java/org/testing/Launch.java
+++ src/test/java/org/testing/Launch.java
@@ -4,18 +4,19 @@ import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
+
 public class Launch {
 	public static void main(String[] args) {
 		 System.setProperty("Webdriver.chrome.driver","C:\\Users\\Divi keerthi\\eclipse workspace\\Project\\lib\\chromedriver.exe");
-		 WebDriver driver=new ChromeDriver();
-		 driver.get("https://www.facebook.com/");
-		 //fghhjd
+		// WebDriver driver=new ChromeDriver();
+		 //driver.get("https://www.facebook.com/");
+		 
 		 WebElement user = driver.findElement(By.id("email"));
 	        user.sendKeys("raj@gmail.com");
 	     WebElement pswd = driver.findElement(By.id("pass"));
 	        pswd.sendKeys("234567");
-         //ssdfd
-	     //djfhfh   
+         WebElement btn = driver.findElement(By.id("loginbutton"));
+           btn.click();
 	}
 
 }
